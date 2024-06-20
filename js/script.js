142127577844f9bb2cc4b00b6e88e6ae10bab0ec
darklowlyx <dark.codx.lowly@gmail.com>
2024-06-20 23:24:54 +0500

    version 0.5

---
+++ js/script.js
@@ -0,0 +1,71 @@
+let items = [];
+
+const itemsDiv = document.getElementById("items"); 
+const input = document.getElementById("itemInput");
+const storageKey = "items";
+
+input.addEventListener("keydown", (event) => {
+	if (event.key === "Enter") {
+		event.preventDefault();
+		document.getElementById("myBtn").click();
+	}
+}); 
+
+function renderItems() {
+	itemsDiv.innerHTML = null;
+
+	for (const [idx, item] of Object.entries(items)) {
+		const container = document.createElement("div");
+		container.style.marginBottom = "10px";
+
+		const text = document.createElement("p");
+		text.style.display = "inline";
+		text.style.marginRight = "20px"; 
+		text.textContent = item;
+
+		const button = document.createElement("button");
+		button.textContent = "×";
+		button.style.fontSize = "2.5rem";
+		button.style.alignSelf = "center"
+		button.onclick = () => removeItem(idx);
+
+		container.appendChild(text);
+		container.appendChild(button);
+
+		itemsDiv.appendChild(container);
+	}
+}
+
+function loadItems() {
+	const oldItems = localStorage.getItem(storageKey);
+	if (oldItems) items = JSON.parse(oldItems);
+	renderItems();
+}
+
+function saveItems() {
+	const stringItems = JSON.stringify(items);
+	localStorage.setItem(storageKey, stringItems);
+}
+
+function addItem() {
+	const value = input.value;
+	if (!value && value.trim() == "") {
+		placeholder = document.getElementById('itemInput').placeholder;
+		document.getElementById('itemInput').placeholder = "You cannot add an empty item!";
+		setTimeout(() => {document.getElementById('itemInput').placeholder = placeholder;}, 1000);
+		return 
+	}
+
+	items.push(value);
+	renderItems();
+	input.value = "";
+	saveItems();
+}
+
+function removeItem(idx) {
+	items.splice(idx, 1);
+	renderItems();
+	saveItems();
+}
+
+document.addEventListener("DOMContentLoaded", loadItems)
